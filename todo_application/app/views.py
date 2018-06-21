f265f0b20ebc4bffce4ca284171cfbbdbe8fd607
jeevan437 <jeevanpjr@gmail.com>
2018-06-21 14:56:55 +0530

    added views , models and urls

--- todo_application/app/views.py
+++ todo_application/app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import todo
 # Create your views here.
+
+def todo_view(request)  :
+
+    data = todo.objects.all()
+
+    return render(request,'todo_details.html',{'todo_data':data})
+
